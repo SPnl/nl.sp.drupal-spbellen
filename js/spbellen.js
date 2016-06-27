c809e4e3cbd681e6ff1a072a5de0c46dbb1894be
Thomas de Graaff <tdgraaff@sp.nl>
2016-06-27 20:47:55 +0200

    Adds javascript which adds review class to body on review webform page.

---
+++ js/spbellen.js
@@ -0,0 +1,9 @@
+(function ($) {
+  $(document).ready(function(){
+    var isPreview = document.getElementsByClassName('webform-client-form preview');
+    if (isPreview.length > 0) {
+      $('body').addClass('preview');
+      console.log("Preview page!");  //here we can add our JS code
+    } 
+  });
+})(jQuery);
