c2d238a584dbc538e8c74e308fc6415140e7c8ab
Thomas de Graaff <tdgraaff@sp.nl>
2016-10-10 15:15:52 +0200

    Adds campaign introduction text option.

--- js/spbellen.js
+++ js/spbellen.js
@@ -1,9 +1,15 @@
 (function ($) {
-  $(document).ready(function(){
+
+Drupal.behaviors.spBellen = {
+  attach: function (context) {
+ 
     var isPreview = document.getElementsByClassName('webform-client-form preview');
     if (isPreview.length > 0) {
       $('body').addClass('preview');
-      console.log("Preview page!");  //here we can add our JS code
+      //console.log("Preview page!");  //here we can add our JS code
     } 
-  });
+
+  } 
+};
+
 })(jQuery);
