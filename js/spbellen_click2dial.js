add46d4b2c74c027822ff67d272486b477eafe6a
Thomas de Graaff <tdgraaff@sp.nl>
2016-12-20 09:08:00 +0000

    Adds click2dial caller number settings per campaign.

--- js/spbellen_click2dial.js
+++ js/spbellen_click2dial.js
@@ -11,22 +11,12 @@ Drupal.behaviors.spBellenClick2Dial = {
      var extension = click2dialParams.extension;
      $('#spbellen-click2dial-button').click(function( event ) {
        event.preventDefault();
-       //console.log( click2dialUrl );
-       //console.log( click2dialParams );
-       //console.log( shortname );
-       //console.log( dest );
-       //console.log( extension );
-       //var click2dial_result = $.get(click2dialUrl, click2dialParams);
        var click2dial_result = $.ajax({
          type: "GET",
          url: click2dialUrl,
          dataType: "html",
          data: click2dialParams,
-         //error: function(request, status, error) { alert( 'Error: ' + request.responseText ); },
-         //success: function(msg){ alert( "Success: " + msg ); }
-       }
-       );
-
+       });
      });
    }
 
