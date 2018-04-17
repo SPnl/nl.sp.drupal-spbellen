59daad3d62c3084e901d0500734c7a188a4566c3
rdrooij <rdrooij@sp.nl>
2018-04-17 14:18:45 +0200

    spbellen.js: remove trailing spaces

--- js/spbellen.js
+++ js/spbellen.js
@@ -2,14 +2,14 @@
 
 Drupal.behaviors.spBellen = {
   attach: function (context) {
- 
+
     var isPreview = document.getElementsByClassName('webform-client-form preview');
     if (isPreview.length > 0) {
       $('body').addClass('preview');
       //console.log("Preview page!");  //here we can add our JS code
-    } 
+    }
 
-  } 
+  }
 };
 
 })(jQuery);
