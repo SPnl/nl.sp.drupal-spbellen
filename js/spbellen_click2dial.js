fac7e909ff446bc8f244790865b4596e746f56d8
Thomas de Graaff <tdgraaff@sp.nl>
2018-08-29 12:28:14 +0200

    Belknop actie: Asterix server ip -> server domain, and http -> https.

--- js/spbellen_click2dial.js
+++ js/spbellen_click2dial.js
@@ -2,7 +2,7 @@
 
 Drupal.behaviors.spBellenClick2Dial = {
   attach: function (context) {
-   
+
    if (Drupal.settings.spbellen) {
      var click2dialUrl = Drupal.settings.spbellen.click2dial.script_url;
      var click2dialParams = Drupal.settings.spbellen.click2dial.script_params;
