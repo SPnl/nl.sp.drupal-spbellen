b20b15487fac9deeb0615b0f3251fdc8f9a75c0e
Thomas de Graaff <tdgraaff@sp.nl>
2018-12-12 16:22:51 +0100

    Prettier tables in caller stats.

--- spbellen-caller-overview.tpl.php
+++ spbellen-caller-overview.tpl.php
@@ -19,54 +19,69 @@
   <table style="width: auto">
     <tr>
       <th></th>
-      <th colspan="2">beller&nbsp;totaal</th>
-      <th colspan="2">beller&nbsp;campagne</th>
-      <th colspan="2">campagne</th>
+      <th colspan="3">beller&nbsp;totaal</th>
+      <th colspan="3">beller&nbsp;campagne</th>
+      <th colspan="3">campagne</th>
     </tr>
     <tr>
       <td width="200">Afgerond</td>
-      <td width="1"><?php print $general_stats_caller['completed']; ?></td>
-      <td><?php print $general_stats_caller['completed_percentage']; ?>%</td>
-      <td width="1"><?php print $general_stats_caller_campaign['completed']; ?></td>
-      <td><?php print $general_stats_caller_campaign['completed_percentage']; ?>%</td>
-      <td width="1"><?php print $general_stats_campaign['completed']; ?></td>
-      <td><?php print $general_stats_campaign['completed_percentage']; ?>%</td>
+      <td align="right" width="1"><?php print $general_stats_caller['completed']; ?></td>
+      <td align="right" width="1"><?php print $general_stats_caller['completed_percentage']; ?>%</td>
+      <td></td>
+      <td align="right" width="1"><?php print $general_stats_caller_campaign['completed']; ?></td>
+      <td align="right" width="1"><?php print $general_stats_caller_campaign['completed_percentage']; ?>%</td>
+      <td></td>
+      <td align="right" width="1"><?php print $general_stats_campaign['completed']; ?></td>
+      <td align="right" width="1"><?php print $general_stats_campaign['completed_percentage']; ?>%</td>
+      <td></td>
     </tr>
     <tr>
       <td>Terugbellen</td>
-      <td><?php print $general_stats_caller['call_back']; ?></td>
-      <td><?php print $general_stats_caller['call_back_percentage']; ?>%</td>
-      <td><?php print $general_stats_caller_campaign['call_back']; ?></td>
-      <td><?php print $general_stats_caller_campaign['call_back_percentage']; ?>%</td>
-      <td><?php print $general_stats_campaign['call_back']; ?></td>
-      <td><?php print $general_stats_campaign['call_back_percentage']; ?>%</td>
+      <td align="right"><?php print $general_stats_caller['call_back']; ?></td>
+      <td align="right"><?php print $general_stats_caller['call_back_percentage']; ?>%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_caller_campaign['call_back']; ?></td>
+      <td align="right"><?php print $general_stats_caller_campaign['call_back_percentage']; ?>%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_campaign['call_back']; ?></td>
+      <td align="right"><?php print $general_stats_campaign['call_back_percentage']; ?>%</td>
+      <td></td>
     </tr>
     <tr>
       <td>Verkeerd nummer</td>
-      <td><?php print $general_stats_caller['wrong_number']; ?></td>
-      <td><?php print $general_stats_caller['wrong_number_percentage']; ?>%</td>
-      <td><?php print $general_stats_caller_campaign['wrong_number']; ?></td>
-      <td><?php print $general_stats_caller_campaign['wrong_number_percentage']; ?>%</td>
-      <td><?php print $general_stats_campaign['wrong_number']; ?></td>
-      <td><?php print $general_stats_campaign['wrong_number_percentage']; ?>%</td>
+      <td align="right"><?php print $general_stats_caller['wrong_number']; ?></td>
+      <td align="right"><?php print $general_stats_caller['wrong_number_percentage']; ?>%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_caller_campaign['wrong_number']; ?></td>
+      <td align="right"><?php print $general_stats_caller_campaign['wrong_number_percentage']; ?>%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_campaign['wrong_number']; ?></td>
+      <td align="right"><?php print $general_stats_campaign['wrong_number_percentage']; ?>%</td>
+      <td></td>
     </tr>
     <tr>
       <td>Nooit meer bellen</td>
-      <td><?php print $general_stats_caller['blacklisted']; ?></td>
-      <td><?php print $general_stats_caller['blacklisted_percentage']; ?>%</td>
-      <td><?php print $general_stats_caller_campaign['blacklisted']; ?></td>
-      <td><?php print $general_stats_caller_campaign['blacklisted_percentage']; ?>%</td>
-      <td><?php print $general_stats_campaign['blacklisted']; ?></td>
-      <td><?php print $general_stats_campaign['blacklisted_percentage']; ?>%</td>
+      <td align="right"><?php print $general_stats_caller['blacklisted']; ?></td>
+      <td align="right"><?php print $general_stats_caller['blacklisted_percentage']; ?>%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_caller_campaign['blacklisted']; ?></td>
+      <td align="right"><?php print $general_stats_caller_campaign['blacklisted_percentage']; ?>%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_campaign['blacklisted']; ?></td>
+      <td align="right"><?php print $general_stats_campaign['blacklisted_percentage']; ?>%</td>
+      <td></td>
     </tr>
     <tr>
       <td>Totaal</td>
-      <td><?php print $general_stats_caller['total']; ?></td>
-      <td>100%</td>
-      <td><?php print $general_stats_caller_campaign['total']; ?></td>
-      <td><?php print $general_stats_caller_campaign['total_percentage']; ?>%</td>
-      <td><?php print $general_stats_campaign['total']; ?></td>
-      <td><?php print $general_stats_campaign['total_percentage']; ?>%</td>
+      <td align="right"><?php print $general_stats_caller['total']; ?></td>
+      <td align="right">100%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_caller_campaign['total']; ?></td>
+      <td align="right"><?php print $general_stats_caller_campaign['total_percentage']; ?>%</td>
+      <td></td>
+      <td align="right"><?php print $general_stats_campaign['total']; ?></td>
+      <td align="right"><?php print $general_stats_campaign['total_percentage']; ?>%</td>
+      <td></td>
     </tr>
   </table>
 <?php endif; ?>
@@ -85,10 +100,10 @@
       <?php foreach ($data['options'] as $key => $option) : ?>
         <tr>
           <td width="200"><?php print($option); ?></td>
-          <td width="1"><?php print($data['stats'][$key]); ?></td>
-          <td><?php print($data['stats'][$key. '_percentage']); ?>%</td>
-          <td width="1"><?php print($result_stats_campaign['components'][$cid]['stats'][$key]); ?></td>
-          <td><?php print($result_stats_campaign['components'][$cid]['stats'][$key . '_percentage']); ?>%</td>
+          <td align="right" width="1"><?php print($data['stats'][$key]); ?></td>
+          <td align="right"><?php print($data['stats'][$key. '_percentage']); ?>%</td>
+          <td align="right" width="1"><?php print($result_stats_campaign['components'][$cid]['stats'][$key]); ?></td>
+          <td align="right"><?php print($result_stats_campaign['components'][$cid]['stats'][$key . '_percentage']); ?>%</td>
         </tr>
       <?php endforeach; ?>
     </table>
