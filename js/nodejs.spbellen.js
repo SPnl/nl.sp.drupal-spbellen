e6a562545ea62a11c21d89520edd8a99daa4b7a1
Thomas de Graaff <tdgraaff@sp.nl>
2019-02-18 15:18:16 +0100

    Adds call button page.

---
+++ js/nodejs.spbellen.js
@@ -0,0 +1,11 @@
+Drupal.Nodejs.callbacks.spbellen_update_phone_number = {
+  callback: function (message) {
+  	//console.log(message);
+  	if (message.callback == 'spbellen_update_phone_number') {
+	    // Do whatever I want on the client's browser from here
+      button = document.getElementById("spbellen-call-button");
+      button.textContent = '----------';
+      setTimeout(function(){button.textContent = message.data.phone_number}, 500);
+  	}
+  }
+};
---
+++ spbellen_call_button.tpl.php
@@ -0,0 +1,3 @@
+<div>
+  <a id="spbellen-call-button" href="tel:<?php print $phone_number; ?>"><?php print $phone_number; ?></a>
+</div>
