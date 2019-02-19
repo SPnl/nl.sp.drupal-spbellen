b5279bb80bdd5b3c8941d00c06f730bc8eedf5ce
Thomas de Graaff <tdgraaff@sp.nl>
2019-02-19 12:34:03 +0100

    Adds call script or button choice on login.

--- spbellen_call_button.tpl.php
+++ spbellen_call_button.tpl.php
@@ -1,3 +1,7 @@
 <div>
-  <a id="spbellen-call-button" href="tel:<?php print $phone_number; ?>"><?php print $phone_number; ?></a>
+  <?php if (empty($phone_number)) : ?>
+    <a id="spbellen-call-button"">Log op uw computer in</a>
+  <?php else : ?>
+    <a id="spbellen-call-button" href="tel:<?php print $phone_number; ?>"><?php print $phone_number; ?></a>
+  <?php endif; ?>
 </div>
