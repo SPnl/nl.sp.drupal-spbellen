f89a4b9c97acb59db0f4a2c7f6e411f873e2f2e3
Thomas de Graaff <tdgraaff@sp.nl>
2019-02-21 17:11:14 +0100

    Fixes bug href call button.

--- js/nodejs.spbellen.js
+++ js/nodejs.spbellen.js
@@ -5,7 +5,7 @@ Drupal.Nodejs.callbacks.spbellen_update_phone_number = {
 	    // Do whatever I want on the client's browser from here
       button = document.getElementById("spbellen-call-button");
       button.textContent = '----------';
-      setTimeout(function(){button.textContent = message.data.phone_number}, 500);
+      setTimeout(function(){button.textContent = message.data.phone_number; button.href = 'tel:' + message.data.phone_number}, 500);
   	}
   }
 };
