f751a4210b53f7fd0e68b2edf01374225987db08
Thomas de Graaff <tdgraaff@sp.nl>
2019-03-05 11:51:25 +0100

    Adds display name to call button.

--- js/nodejs.spbellen.js
+++ js/nodejs.spbellen.js
@@ -5,7 +5,9 @@ Drupal.Nodejs.callbacks.spbellen_update_phone_number = {
 	    // Do whatever I want on the client's browser from here
       button = document.getElementById("spbellen-call-button");
       button.textContent = '----------';
-      setTimeout(function(){button.textContent = message.data.phone_number; button.href = 'tel:' + message.data.phone_number}, 500);
+      display_name = document.getElementById("spbellen-display-name");
+      display_name.textContent = '                      ';
+      setTimeout(function(){button.textContent = message.data.phone_number; button.href = 'tel:' + message.data.phone_number; display_name.textContent = message.data.display_name}, 500);
   	}
   }
 };
--- spbellen_call_button.tpl.php
+++ spbellen_call_button.tpl.php
@@ -3,5 +3,6 @@
     <a id="spbellen-call-button"">Log op uw computer in</a>
   <?php else : ?>
     <a id="spbellen-call-button" href="tel:<?php print $phone_number; ?>"><?php print $phone_number; ?></a>
+    <span id="spbellen-display-name"><?php print $display_name; ?></span>
   <?php endif; ?>
 </div>
