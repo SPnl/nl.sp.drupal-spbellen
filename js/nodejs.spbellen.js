d96b772c8d4bc4c68a06d4e882c9f479aa4e73fc
Thomas de Graaff <tdgraaff@sp.nl>
2019-03-05 12:08:20 +0100

    Textual improvement display button.

--- js/nodejs.spbellen.js
+++ js/nodejs.spbellen.js
@@ -6,7 +6,7 @@ Drupal.Nodejs.callbacks.spbellen_update_phone_number = {
       button = document.getElementById("spbellen-call-button");
       button.textContent = '----------';
       display_name = document.getElementById("spbellen-display-name");
-      display_name.textContent = '                      ';
+      display_name.textContent = '------------------';
       setTimeout(function(){button.textContent = message.data.phone_number; button.href = 'tel:' + message.data.phone_number; display_name.textContent = message.data.display_name}, 500);
   	}
   }
